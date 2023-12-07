f2351c87f7c670734e625f8fe35ce96df9d80cf0
Victor Irad Verdeja Trujillo <96750594+CodedSpirit-dev@users.noreply.github.com>
2023-12-06 18:51:32 -0600

    Last commit before online classs

--- src/public components/Events/index.jsx
+++ src/public components/Events/index.jsx
@@ -4,7 +4,6 @@ import EventItem from "./components/EventItem";
 const Events = ({ searchTerm }) => {
   const { events } = useEventsData();
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleEventClick = (id) => {
     console.log("evento clickeado", id);
   };
